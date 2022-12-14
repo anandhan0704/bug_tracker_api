c1ccb6c6314d8d99a655832f36b96af5f4dede6f
Anandha Krishnan <anandha.krishnan@urolime.com>
2022-12-14 23:34:33 +0530

    Websocket test

--- src/routes/index.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const healthRoutes = require('./health.route');
-
-router.use('/health', healthRoutes);
-
-module.exports = router;

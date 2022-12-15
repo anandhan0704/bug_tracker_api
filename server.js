27196ccec4700a6e63d32d20b36cada90b2269ad
Anandha Krishnan <anandha.krishnan@urolime.com>
2022-12-15 16:21:27 +0530

    ws send

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const wss = new WebSocket.Server({ port: 8080 })
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
-    ws.send('Hello! Message From Server!!', `${message}`)
   })
   ws.onmessage = (message) => {
     console.log(message);

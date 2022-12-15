0e26231dde72cfb2609e3da1fa258fb990885c39
Anandha Krishnan <anandha.krishnan@urolime.com>
2022-12-15 22:19:42 +0530

    Emit message

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const wss = new WebSocket.Server({ port: 8080 })
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
-    ws.send(`Received message => ${message}`)
+    ws.emit('message', message);
   })
   ws.onmessage = (message) => {
     console.log(message);
   };
+
+  ws.addEventListener("open", (message) => {
+        console.log("We are connected");
+        ws.send(message);
+      });
 })
 
 console.log("Server connected on 8080")

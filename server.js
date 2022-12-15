59f3a8331f41f7dfdb4171f56d6025290d1d9230
Anandha Krishnan <anandha.krishnan@urolime.com>
2022-12-15 16:02:48 +0530

    ws send

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: 4000 })
+const wss = new WebSocket.Server({ port: 8080 })
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
@@ -9,3 +9,5 @@ wss.on('connection', ws => {
   };
   ws.send('Hello! Message From Server!!', `${message}`)
 })
+
+console.log("Server connected on 8080")

856c816dfbf65dbb70552bd55d49097b3b92a26e
Anandha Krishnan <anandha.krishnan@urolime.com>
2022-12-15 18:09:45 +0530

    Data to client

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const wss = new WebSocket.Server({ port: 8080 })
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
+    ws.send(`Received message => ${message}`)
   })
   ws.onmessage = (message) => {
     console.log(message);
   };
-  ws.send('Hello! Message From WS!!')
 })
 
 console.log("Server connected on 8080")

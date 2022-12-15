c4c25fbc039ce37323f5c55f63a1b706f9c75ead
Anandha Krishnan <anandha.krishnan@urolime.com>
2022-12-15 23:22:36 +0530

    Emit message for client

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 8080 })
 wss.on('connection', ws => {
   ws.on('message', message => {
-    console.log(`Received message => ${message}`)
-    ws.send(`Received message => ${message}`)
+    ws.send(message)
   })
   ws.onmessage = (message) => {
     console.log(message);

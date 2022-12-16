1cc049cbdc71dff9dd7feeb4fdcabd7c6e78ff5e
Anandha Krishnan <anandha.krishnan@urolime.com>
2022-12-16 13:58:40 +0530

    Websocket

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: 8080 })
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    console.log(`Received message => ${message}`)
-    ws.send('Hello! Message From Server!!')
-  })
-})
+const WebSocket = require('ws');
+var port = 8080;
+const wss = new WebSocket.Server({ "port": port });
 
-console.log("Server connected on 8080")
+// Broadcast to all.
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if ( client.readyState == WebSocket.OPEN && data != undefined ) 
+      client.send(data);
+  });
+};
+
+wss.on('connection', function connection(ws) {
+  console.log("CONNECTION OK...");
+  ws.on('message', function incoming(data) {
+    // Broadcast to everyone else.
+    console.log(data.toString())
+    wss.broadcast(data);
+  });
+});
+
+console.log((new Date()) + ' Server is listening on port 8080');
